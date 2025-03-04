ed61622394f0734f5340584ef583c0a9ddbaffb5
Dmitry <a.jelousguy@gmail.com>
2025-03-04 17:34:14 +0300

    another commit

--- src/main/java/spring/controller/GroupController.java
+++ src/main/java/spring/controller/GroupController.java
@@ -1,7 +1,7 @@
 package spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
