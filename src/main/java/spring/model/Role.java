16b32209e3acaab0fc13fc0c5051b6bbb1679216
Dmitry <a.jelousguy@gmail.com>
2025-07-06 15:25:44 +0300

    add extras

--- src/main/java/spring/model/Role.java
+++ src/main/java/spring/model/Role.java
@@ -3,9 +3,8 @@ package spring.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-	
-	ADMIN,
-	USER;
+
+	ADMIN, USER;
 
 	@Override
 	public String getAuthority() {
